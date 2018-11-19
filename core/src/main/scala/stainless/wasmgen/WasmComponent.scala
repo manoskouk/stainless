20730950fc8591357c68835ed590fe3b33dce069
Manos Koukoutos <emmanouil.koukoutos@epfl.ch>
2018-11-19 17:03:40 +0100

    Add wasm Component

---
+++ core/src/main/scala/stainless/wasmgen/WasmComponent.scala
@@ -0,0 +1,52 @@
+/* Copyright 2009-2018 EPFL, Lausanne */
+
+package stainless
+package wasmgen
+
+import inox.Context
+import inox.transformers.SymbolTransformer
+import extraction.StainlessPipeline
+import scala.concurrent.Future
+
+object DebugSectionWasm extends inox.DebugSection("wasm")
+
+class WasmAnalysis extends AbstractAnalysis {
+  val name = "no analysis"
+  type Report = NoReport
+
+  def toReport = new NoReport
+}
+
+object WasmComponent extends Component {
+  val name = "wasm-codegen"
+  val description = "Generate WebAssembly code that runs parameterless functions in the program"
+  type Report = NoReport
+  type Analysis = WasmAnalysis
+
+  override val lowering: SymbolTransformer {
+    val s: extraction.trees.type
+    val t: extraction.trees.type
+  } = inox.transformers.SymbolTransformer(new transformers.TreeTransformer {
+    val s: extraction.trees.type = extraction.trees
+    val t: extraction.trees.type = extraction.trees
+  })
+
+  def run(pipeline: StainlessPipeline)(implicit context: Context) =
+    new WasmComponentRun(pipeline)(context)
+}
+
+class WasmComponentRun(override val pipeline: StainlessPipeline)
+                      (override implicit val context: inox.Context) extends {
+  override val component = WasmComponent
+  override val trees: stainless.trees.type = stainless.trees
+} with ComponentRun {
+
+  def parse(json: io.circe.Json): component.Report = new NoReport
+
+  private[stainless] def apply(functions: Seq[Identifier], symbols: trees.Symbols): Future[WasmAnalysis] = {
+    Future.successful {
+      wasm.Printer(codegen.LinearMemoryCodeGen.transform(intermediate.RecordAbstractor.transform(symbols)))
+      new WasmAnalysis
+    }
+  }
+}
